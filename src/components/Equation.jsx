e71040429aaea2f0bccf100ac94e3138ceb9778a
Benjamin Laver <laverben2@gmail.com>
2025-07-29 00:56:42 +0100

    Update Equation Number Alignment

--- src/components/Equation.jsx
+++ src/components/Equation.jsx
@@ -1,21 +1,29 @@
-import { BlockMath } from 'react-katex';
-import 'katex/dist/katex.min.css';
+import { BlockMath } from "react-katex";
+import "katex/dist/katex.min.css";
 
 function Equation({ children, number, id }) {
   return (
     <div
       id={id}
       style={{
-        backgroundColor: 'rgba(236, 185, 75, 0.38)',
-        display: 'grid',
-        gridTemplateColumns: '1fr auto',
-        alignItems: 'center',
-        width: '100%',
+        backgroundColor: "rgba(236, 185, 75, 0.38)",
+        display: "grid",
+        gridTemplateColumns: "1fr auto",
+        alignItems: "center",
+        width: "100%",
       }}
     >
-      <BlockMath style={{ justifySelf: 'center' }}>{children}</BlockMath>
+      <BlockMath style={{ justifySelf: "center" }}>{children}</BlockMath>
       {number && (
-        <span style={{ marginLeft: '0.5rem', justifySelf: 'end' }}>({number})</span>
+        <span
+          style={{
+            marginLeft: "0.5rem",
+            marginRight: "0.5rem",
+            justifySelf: "end",
+          }}
+        >
+          ({number})
+        </span>
       )}
     </div>
   );
